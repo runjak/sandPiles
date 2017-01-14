364f8fff94570a314886f187283baf2fa6864637
Jakob Runge <sicarius@g4t3.de>
2017-01-14 23:20:15 +0100

    Introduce canvas in SandPileView

--- src/views/SandPileView.js
+++ src/views/SandPileView.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
+import SandPileActions from '../data/SandPileActions';
+import './SandPileView.css';
 
 class SandPileView extends Component {
+  componentDidMount(){
+    this.updateCanvas();
+  }
+
+  componentDidUpdate(){
+    var canvas = this.refs.canvas;
+    SandPileActions.initSandPile(canvas.scrollWidth, canvas.srollHeight);
+  }
+
+  updateCanvas(){
+    const ctx = this.refs.canvas.getContext('2d');
+  }
+
   render(){
-    return (<div>Yeah!</div>);
+    return (<canvas className="canvas" ref="canvas" />);
   }
 }
 
