1e82f7be8ccd18aae1aa66dc70be1db54eb532e8
Jakob Runge <sicarius@g4t3.de>
2017-01-15 21:36:31 +0100

    Remove module `App`
    - It was only a proxy for the SandPileContainer in the end.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import SandPileContainer from './containers/SandPileContainer';
 
 ReactDOM.render(
-  <App />,
+  <SandPileContainer />,
   document.getElementById('root')
 );
