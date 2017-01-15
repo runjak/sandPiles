d96756bf4c9192d9c8742981d30e0dbe6e343f32
Jakob Runge <sicarius@g4t3.de>
2017-01-15 17:31:01 +0100

    Add data/SandPile:addToPile(…)

--- src/data/SandPile.js
+++ src/data/SandPile.js
@@ -8,6 +8,13 @@ export function mergePiles(p1, p2){
   return p1.clone().add(p2);
 }
 
+export function addToPile(pile, x, y, amount){
+  const currentSize = pile.size();
+  const addPile = math.zeros(currentSize[0], currentSize[1]);
+  math.subset(addPile, x, y, amount);
+  return mergePiles(pile, addPile);
+}
+
 export function valToColor(v){
   switch(v){
     default:
